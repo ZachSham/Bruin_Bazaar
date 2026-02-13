13135db91f4ebad1b6865576feab5f8c9fe07034
Zach Shamieh <zachshamieh@gmail.com>
2026-02-13 12:15:31 -0800

    quick change to mongo connection

--- server/connection.js
+++ server/connection.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
+    console.log('"Pinged your deployment. You successfully connected to MongoDB!"');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
     process.exit(1);
