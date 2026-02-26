2c22b89c6d17013b53557abd6ca5d26867a2d3da
Rhett Selby <rhettselby10@gmail.com>
2026-02-26 13:11:03 -0800

    messages + conversations models

---
+++ server/models/messages.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema({
+    conversation: { type: mongoose.Schema.Types.ObjectId, ref: 'Conversation', required: true },
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    content: { type: String, required: true },
+}, { timestamps: true })
+
+
+const Message = mongoose.model('User', userSchema);
+export default Message;
