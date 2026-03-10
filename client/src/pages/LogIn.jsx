f39490278d1b18deb013b19bcc52fc6c53365697
Jimin Cha <jmchaa06@gmail.com>
2026-03-10 00:49:15 -0700

    Fixed import to match filename exactly.

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Login.css';
+import './LogIn.css';
 import Header from '../components/Header';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
