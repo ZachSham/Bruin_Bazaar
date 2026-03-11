3b78d68a19b6038bc3da326f9812403f5018f31f
ashleytran3 <ashlleynt@gmail.com>
2026-03-11 14:22:58 -0700

    Fixed image sizing

--- client/src/components/ListingCard.jsx
+++ client/src/components/ListingCard.jsx
@@ -5,7 +5,9 @@ function ListingCard({title, price, description, seller, images = [], onClick}) 
     const imageUrl = images?.length > 0 ? images[0] : 'https://placehold.co/300x200?text=No+Image';
     return (
         <div className="listing-item" onClick={onClick}>
-            <img src={imageUrl} alt={title} />
+            <div className="listing-img">
+                <img src={imageUrl} alt={title} />
+            </div>
             <div className="listing-descrip">
                 <h1>${price}</h1>
                 <p className="listing-title">{title}</p>
